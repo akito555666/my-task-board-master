a47822903a643c237412c4a1f2533184217204f8
aki0018 <akito555666@gmail.com>
2025-10-25 09:41:13 +0900

    fix design

--- src/TaskBoard.tsx
+++ src/TaskBoard.tsx
@@ -3,19 +3,83 @@ import { JSX } from "react";
 export const TaskBoard = (): JSX.Element => {
   return (
     <>
+      <div className="board-header">
+        <div className="board-app-icon">📄</div>
+        <div className="board-text">
+          <div className="board-title">My Task Board
+            <div className="board-edit-icon">✏️</div>
+          </div>
+          <div className="board-description">Tasks to keep organised</div>
+        </div>
+      </div>
+
       <div className="task-board">
-        <div className="task-board-name">My Task Board</div>
-        <div className="task-board-description">Tasks to keep organised</div>
         {/* ここにタスクボードの内容を追加します */}
         {/* <div>Enter a short description</div>
       <div>In Progress</div>
       <div>Completed</div>
       <div>Won’t do</div> */}
-        <div className="task-board-items task-in-progress">Task in Progress</div>
-        <div className="task-board-items task-completed">Task Completed</div>
-        <div className="task-board-items task-wont-do">Task Won’t Do</div>
-        <div className="task-board-items task-todo">Task To Do</div>
-        <div className="task-board-items task-add-new">Add new task</div>
+        <div className="task-card in-progress">
+          <div className="task-header">
+            <span className="task-icon">⏰️</span>
+            <span className="task-name">Task in Progress</span>
+            <span className="task-status progress">⏳️</span>
+          </div>
+          <div className="task-content">
+          </div>
+        </div>
+        <div className="task-card completed">
+          <div className="task-header">
+            <span className="task-icon">🏋️‍♂️</span>
+            <span className="task-name">Task Completed</span>
+            <span className="task-status complete">✔</span>
+          </div>
+          <div className="task-content">
+          </div>
+        </div>
+        <div className="task-card wont-do">
+          <div className="task-header">
+            <span className="task-icon">☕</span>
+            <span className="task-name">Task Won’t Do</span>
+            <span className="task-status cancel">❌️</span>
+          </div>
+          <div className="task-content">
+          </div>
+        </div>
+        <div className="task-card to-do">
+          <div className="task-header">
+            <span className="task-icon">📚</span>
+            <span className="task-name">Task To Do</span>
+          </div>
+          <div className="task-content">
+            Work on a Challenge on devchallenges.io,<br /> to
+            learn TypeScript.
+          </div>
+        </div>
+        <div className="task-card add-task">
+          <div className="task-header">
+            <span className="task-icon">➕️</span>
+            <span className="task-add">Add new task</span>
+          </div>
+        </div>
+
+        {/* <div id="1">
+          <div className="task-icon">⏰️</div>
+          <button className="task-card" onClick={() => { alert("Click Task in Progress") }}>Task in Progress</button>
+        </div>
+        <div id="2">
+          <div className="task-icon">🏋️‍♂️</div>
+          <button className="task-card" onClick={() => { alert("Click Task Completed") }}>Task Completed</button>
+        </div>
+        <div id="3">
+          <div className="task-icon">☕</div>
+          <button className="task-card" onClick={() => { alert("Click Task Won’t Do") }}>Task Won’t Do</button>
+        </div>
+        <div id="4">
+          <div className="task-icon">📚</div>
+          <button className="task-card" onClick={() => { alert("Click Task To Do") }}>Task To Do</button>
+        </div>
+        <button className="task-add-new" onClick={() => { alert("Add new task") }}>Add new task</button> */}
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // @ts-ignore: allow importing CSS for side effects without types
-import './index.css';
+import './style.css';
 import { TaskBoard } from './TaskBoard.tsx';
 
 createRoot(document.getElementById('root')!).render(
