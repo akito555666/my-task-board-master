b22e780c28c772d53c904154bb326e22c675f3a6
aki0018 <akito555666@gmail.com>
2025-11-25 12:26:24 +0900

    change レコードIDを自動採番に変更

--- server/init.sql
+++ server/init.sql
@@ -3,16 +3,16 @@ DROP TABLE IF EXISTS boards CASCADE;
 
 -- boardsテーブル
 CREATE TABLE boards (
-    id VARCHAR(255) PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT
 );
 
 -- tasksテーブル
 CREATE TABLE tasks (
-    id VARCHAR(255) PRIMARY KEY,
-    board_id VARCHAR(255) NOT NULL REFERENCES boards(id) ON DELETE CASCADE,
-    name VARCHAR(255) NOT NULL DEFAULT 'Untitled Task',
+    id SERIAL PRIMARY KEY,
+    board_id INTEGER NOT NULL REFERENCES boards(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL DEFAULT '',
     icon VARCHAR(255) DEFAULT '',
     status_name VARCHAR(50) NOT NULL DEFAULT '',
     content TEXT DEFAULT '',
